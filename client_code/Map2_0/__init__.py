ef680ed47c7d76a50e55cb99e672a1c98797bb25
kevleh95@googlemail.com <kevleh95@googlemail.com>
2021-11-02 13:21:51 +0000

    Edited Map2_0

--- client_code/Map2_0/__init__.py
+++ client_code/Map2_0/__init__.py
@@ -1117,7 +1117,7 @@ class Map2_0(Map2_0Template):
       'type': 'fill',
       'source': {
           'type': 'geojson',
-          'data': 'https://raw.githubusercontent.com/ShinyKampfkeule/geojson_germany/main/GemeindenHigh.geojson'
+          'data': 'https://raw.githubusercontent.com/CBKLehmann/Geodata/main/gemeinden.geojson'
       },
       'layout': {
           'visibility': 'none'
@@ -1139,7 +1139,7 @@ class Map2_0(Map2_0Template):
         'type': 'line',
         'source': {
             'type': 'geojson',
-            'data': 'https://raw.githubusercontent.com/ShinyKampfkeule/geojson_germany/main/GemeindenHigh.geojson'
+            'data': 'https://raw.githubusercontent.com/CBKLehmann/Geodata/main/gemeinden.geojson'
         },
         'layout': {
             'visibility': 'none'
