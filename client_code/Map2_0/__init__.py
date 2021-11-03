0292fb4b259ac651a6e9f9132a9145b16733f412
kevleh95@googlemail.com <kevleh95@googlemail.com>
2021-11-03 10:34:02 +0000

    Edited Map2_0

--- client_code/Map2_0/__init__.py
+++ client_code/Map2_0/__init__.py
@@ -60,55 +60,6 @@ class Map2_0(Map2_0Template):
     self.mapbox.on('click', 'gemeinden', self.popup)
     self.mapbox.on('click', self.poi)
     self.mapbox.on('styledata', self.place_layer)
-
-    #***POI-Layer***
-    dlen = anvil.server.call('get_len_of_features2')
-  
-    i = 0
-    check = 0
-    gm_id = 0
-    data = []
-  
-    #while check < dlen:
-    while check < 50000:
-    
-      #Create index-variable
-      j = 0 
-    
-      #Get data-pack from geojson
-      gm = anvil.server.call('get_json_bh', i)
-    
-      #Increase Value of Data-Variable
-      i += 50000
-  
-      #Check if index-variable is smaller than amount of data-pack
-      while j < len(gm):
-  
-        #Append data-pack to local data
-        data.append(gm[j])
-      
-        #Increase index-variable
-        j += 1
-        gm_id += 1
-      
-      #Get new value for municipalities-amount  
-      check = len(data)
-  
-    for el in data:
-
-      lnglat = [el['lon'], el['lat']]
-    
-      html_el = document.createElement('div')
-    
-      html_el.className = 'icon'
-      html_el.style.backgroundImage = f'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Zeichen_224_-_Haltestelle%2C_StVO_2017.svg/2048px-Zeichen_224_-_Haltestelle%2C_StVO_2017.svg.png)'
-      html_el.style.width = '20px'
-      html_el.style.height = '20px'
-      html_el.style.backgroundSize = '100%'
-      html_el.style.backgroundrepeat = 'no-repeat'
-    
-      poi_marker = mapboxgl.Marker(html_el).setLngLat(lnglat).addTo(self.mapbox)
-    #***End of POI-Layer***
     
   #This method is called when the Geocoder was used 
   def move_marker(self, result):
@@ -1219,3 +1170,19 @@ class Map2_0(Map2_0Template):
       
       self.mapbox.setLayoutProperty('gemeinden', 'visibility', 'visible')
       self.mapbox.setLayoutProperty('outlineGM', 'visibility', 'visible')
+
+  def check_box_1_change(self, **event_args):
+    
+    raw_bbox = self.mapbox.getBounds()
+    
+    lng1 = (dict(raw_bbox['_sw']))['lng']
+    lng2 = (dict(raw_bbox['_ne']))['lng']
+    lat1 = (dict(raw_bbox['_sw']))['lat']
+    lat2 = (dict(raw_bbox['_ne']))['lat']
+  
+    bbox = [(dict(raw_bbox['_sw']))['lat'], (dict(raw_bbox['_sw']))['lng'], (dict(raw_bbox['_ne']))['lat'], (dict(raw_bbox['_ne']))['lng']]
+  
+    print (bbox)
+    
+    anvil.server.call('poi_data', 'health', bbox)
+
