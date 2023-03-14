0353115221f7f74e17115558d11e9dc93289f6a6
kevin.lehmann@capitalbay.de <kevin.lehmann@capitalbay.de>
2023-03-14 10:47:04 +0000

    Edited Map2_0.Comp_Sort

--- client_code/Map2_0/Comp_Sort/__init__.py
+++ client_code/Map2_0/Comp_Sort/__init__.py
@@ -12,8 +12,39 @@ class Comp_Sort(Comp_SortTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    entries = properties['data']
-    print(entries.keys())
-    self.city_dropdown.items = entries.keys()
+    self.entries = properties['data']
+    self.city_dropdown.items = self.entries.keys()
 
-    # Any code you write here will run before the form opens.
+  def refresh_amount(self, **event_args):
+    city_selected = False
+    category_selected = False
+    
+    if not self.city_dropdown.selected_value == None:
+      city = self.city_dropdown.selected_value
+      city_selected = True
+
+    if not self.category_dropdown.selected_value == None:
+      category = self.category_dropdown.selected_value
+      category_selected = True
+
+    if city_selected and category_selected:
+      self.amount = len(self.entries[city][category])
+    elif city_selected:
+      keys = self.entries[city].keys()
+      self.amount = 0
+      for key in keys:
+        self.amount += len(self.entries[city][key])
+    else:
+      self.amount = 0
+
+    if not self.custom_input.text == None:
+      if self.custom_switch.selected and self.custom_input.text <= self.amount:
+        self.amount = self.custom_input.text
+        
+    self.competitor_count.text = f"Found Competitor Amount: {self.amount}"
+
+  def confirm_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    
+    pass
+
