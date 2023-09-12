e7496bb45a0509310182f108b957008a8e067edd
kevin.lehmann@capitalbay.de <kevin.lehmann@capitalbay.de>
2023-09-12 08:57:44 +0000

    Edited Map2_0

--- client_code/Map2_0/__init__.py
+++ client_code/Map2_0/__init__.py
@@ -4443,7 +4443,7 @@ class Map2_0(Map2_0Template):
       al_home_address = Variables.home_address_al
       nh_sorted_coords = nh_data['sorted_coords']
       al_sorted_coords = al_data['sorted_coords']
-
+      
       for entry in nh_home_address:
         if entry in nh_sorted_coords:
           nh_home_index = nh_sorted_coords.index(entry)
@@ -4479,16 +4479,21 @@ class Map2_0(Map2_0Template):
       last_coord_dist = 0
 
       for coordinate in nh_sorted_coords:
+        print('###########################')
+        print(complete_counter)
+        print(coordinate)
         if not 'home' in coordinate:
+          print('Not Home')
           if complete_counter <= 20 and not last_coord_dist == coordinate[1]:
+            print('Under 20 and not last coordinate')
             counter += 1
-            icon = f'{counter} Nursing@0.75x.png'
+            icon = f'{counter}Nursing@0.75x.png'
             for al_coordinate in al_sorted_coords:
               if coordinate[1] == al_coordinate[1]:
                 icon = f'Nursing{counter}@0.75x.png'
             url = f'https%3A%2F%2Fraw.githubusercontent.com/ShinyKampfkeule/geojson_germany/main/{icon}'
             encoded_url = url.replace("/", "%2F")
-            if complete_counter == len(nh_sorted_coords) - 1:
+            if complete_counter == len(nh_sorted_coords) - 1 or complete_counter == 20:
                 if not coordinate[0]['coords'] == last_coords and not 'home' in coordinate:
                   if not counter == 1:
                     request_static_map += f"%2C"
@@ -4499,6 +4504,7 @@ class Map2_0(Map2_0Template):
                 url = f'https%3A%2F%2Fraw.githubusercontent.com/ShinyKampfkeule/geojson_germany/main/PinCBx075.png'
                 encoded_url = url.replace("/", "%2F")
                 request_static_map += f"%7B%22type%22%3A%22Feature%22%2C%22properties%22%3A%7B%22marker%2Durl%22%3A%22{encoded_url}%22%7D%2C%22geometry%22%3A%7B%22type%22%3A%22Point%22%2C%22coordinates%22%3A%5B{nh_data['marker_coords']['lng']},{nh_data['marker_coords']['lat']}%5D%7D%7D%5D%7D"
+                print('Inside Nursing')
                 request.append(request_static_map)
                 request_static_map = request_static_map_raw
                 index_coords -= 1
@@ -4536,7 +4542,7 @@ class Map2_0(Map2_0Template):
       index_coords -= test_counter
 
       last_coord_dist = 0
-
+      
       for coordinate in al_sorted_coords:
         if not 'home' in coordinate:
           if complete_counter <= 20 and not last_coord_dist == coordinate[1]:
@@ -4558,6 +4564,7 @@ class Map2_0(Map2_0Template):
                 url = f'https%3A%2F%2Fraw.githubusercontent.com/ShinyKampfkeule/geojson_germany/main/PinCBx075.png'
                 encoded_url = url.replace("/", "%2F")
                 request_static_map += f"%7B%22type%22%3A%22Feature%22%2C%22properties%22%3A%7B%22marker%2Durl%22%3A%22{encoded_url}%22%7D%2C%22geometry%22%3A%7B%22type%22%3A%22Point%22%2C%22coordinates%22%3A%5B{al_data['marker_coords']['lng']},{al_data['marker_coords']['lat']}%5D%7D%7D%5D%7D"
+                print('Inside Assisted')  
                 request.append(request_static_map)
                 request_static_map = request_static_map_raw
                 index_coords -= 1
@@ -4572,6 +4579,8 @@ class Map2_0(Map2_0Template):
             
           complete_counter += 1
           last_coords = coordinate[0]['coords']
+
+      print(len(request))
       
       if request == []:
             url = f'https%3A%2F%2Fraw.githubusercontent.com/ShinyKampfkeule/geojson_germany/main/PinCBx075.png'
