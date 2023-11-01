fff5609e2639038d3f5dcb350c7efcde18cd83f3
kevin.lehmann@capitalbay.de <kevin.lehmann@capitalbay.de>
2023-11-01 09:25:54 +0000

    Created beach-2562563.jpg, draufsicht-auf-sand-der-meerwasser-trifft.jpg, Login_new and Login_copy; Edited theme.css, settings, Login and theme/parameters.yaml

---
+++ client_code/Login_new/__init__.py
@@ -0,0 +1,16 @@
+from ._anvil_designer import Login_newTemplate
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Login_new(Login_newTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
