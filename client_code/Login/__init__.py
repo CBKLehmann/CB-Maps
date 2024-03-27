1df6d1f670633872e08d44a311634973605b0f00
kevin.lehmann@capitalbay.de <kevin.lehmann@capitalbay.de>
2024-03-27 10:44:26 +0000

    Edited settings, Login and handle_login

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -13,46 +13,58 @@ class Login(LoginTemplate):
   def __init__(self, **properties):
     with anvil.server.no_loading_indicator:
       self.init_components(**properties)
+      
       Functions.create_loading_overlay()
       self.user = anvil.server.call('get_current_user')
-      # self.user = None
       self.hash = get_url_hash()
 
   def form_show(self, **event_args):
     if self.user is not None:
       Variables.user_role = self.user['role']
-      open_form('Map2_0', role=self.user['role'])
-    else:
-      if len(self.hash) > 0:
-        open_form('Map2_0', role='guest')
-      else:
-        self.login_main_grid.visible = True
-        width = anvil.js.window.innerWidth if anvil.js.window.innerWidth > 0 else anvil.js.screen.width;
-        height = anvil.js.window.innerHeight if anvil.js.window.innerHeight > 0 else anvil.js.screen.height;
-        if width <= 998:
-          self.email_icon.visible = False
-          self.password_icon.visible = False
+      open_form('Map2_0')
+      return
+
+    if len(self.hash) > 0:
+      open_form('Map2_0', role='guest')
+      return
+
+    self.display_login_form()
+
+  def display_login_form(self):
+    self.login_main_grid.visible = True
+    width = anvil.js.window.innerWidth if anvil.js.window.innerWidth > 0 else anvil.js.screen.width;
+    height = anvil.js.window.innerHeight if anvil.js.window.innerHeight > 0 else anvil.js.screen.height;
     
+    if width <= 998:
+      self.email_icon.visible = False
+      self.password_icon.visible = False
+  
   def login_click(self, **event_args):
     with anvil.server.no_loading_indicator:
       Functions.manipulate_loading_overlay(True)
-      try:
-        if self.passwort_input.text == "123456":
-          self.forgot_password.raise_event('click')
-          Functions.manipulate_loading_overlay(False)
-          alert('Please update your password using the link in the email you received.', dismissible=False, large=True, role='custom_alert_big')
-        else:
-          # self.user = anvil.users.login_with_email(self.email_input.text, self.passwort_input.text, remember=self.remember_me.checked)
-          self.user = anvil.users.login_with_email(self.email_input.text, self.passwort_input.text, remember=False)
-          if self.user:
-            Variables.user_role = self.user['role']
-            open_form('Map2_0')
-      except anvil.users.AuthenticationFailed:
-          self.error.visible = True
-          Functions.manipulate_loading_overlay(False)
-
+      
+      if self.passwort_input.text == "123456":
+        self.forgot_password.raise_event('click')
+        Functions.manipulate_loading_overlay(False)
+        alert('Please update your password using the link in the email you received.', dismissible=False, large=True, role='custom_alert_big')
+        return
+        
+      self.user = anvil.server.call('login_user', email=self.email_input.text, password=self.passwort_input.text, remember=self.remember_me.checked)
+      if self.user is None:
+        self.error.visible = True
+        Functions.manipulate_loading_overlay(False)
+        return
+        
+      Variables.user_role = self.user['role']
+      open_form('Map2_0')
+          
   def forgot_password_click(self, **event_args):
     with anvil.server.no_loading_indicator:
-      reset = anvil.users.send_password_reset_email(self.email_input.text)
-      self.error.text = "Reset Link has been send to given Email if Email exists"
+      response = anvil.server.call('handle_password_reset', self.email_input.text)  
+      if response == 200:
+        self.error.text = "Reset Link has been send to given Email if Email exists"
+        self.error.visible = True
+        return
+
+      self.error.text = "Given E-Mail not found"
       self.error.visible = True
--- server_code/handle_login.py
+++ server_code/handle_login.py
@@ -3,4 +3,26 @@ import anvil.server
 
 @anvil.server.callable
 def get_current_user():
-  return anvil.users.get_user(allow_remembered=False)
+  user = anvil.users.get_user()
+  
+  if user is not None and user['remember_me']:
+    return user
+
+  return None
+
+@anvil.server.callable
+def login_user(email, password, remember):
+  try:
+    user = anvil.users.login_with_email(email, password, remember=remember)
+    user['remember_me'] = remember
+    return user
+  except anvil.users.AuthenticationFailed:
+    return None
+
+@anvil.server.callable
+def handle_password_reset(email):
+  try:
+    anvil.users.send_password_reset_email(email)
+    return 200
+  except anvil.users.AuthenticationFailed:
+    return 404
