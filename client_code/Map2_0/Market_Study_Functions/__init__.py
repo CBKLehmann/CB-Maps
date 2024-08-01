21b2c2c2936ecc3f8912b89d0c9be4ac633bbb58
kevin.lehmann@capitalbay.de <kevin.lehmann@capitalbay.de>
2024-08-01 14:05:34 +0000

    Edited Map2_0.Market_Study_Functions

--- client_code/Map2_0/Market_Study_Functions/__init__.py
+++ client_code/Map2_0/Market_Study_Functions/__init__.py
@@ -108,9 +108,16 @@ def generate_market_studies(application):
         if market_study_dictionary['district'] == "n.a.":
             market_study_dictionary['district'] = market_study_dictionary['city']
         
+        ##### Temporary Fix #####
+        if market_study_dictionary['city'] == "Fürstenwalde":
+            countie_data_city = "Fürstenwalde/Spree, Stadt"
+        else:
+            countie_data_city = market_study_dictionary['city']
+        ##### Temporary Fix #####
+        
         market_study_dictionary['countie_data'] = anvil.server.call(
             "get_demographic_district_data",
-            city=market_study_dictionary['city']
+            city=countie_data_city
         )
         market_study_dictionary['care_data_district'] = anvil.server.call("get_care_district_data", dist_key=market_study_dictionary['countie_data']['ex_dem_lk']['key'])
         market_study_dictionary['regulations'] = anvil.server.call('read_regulations', federal_state=market_study_dictionary['federal_state'], version="english")
