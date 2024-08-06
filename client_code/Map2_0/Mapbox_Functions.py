2197e145a7827b603d629bcc7cae25911361878c
kevin.lehmann@capitalbay.de <kevin.lehmann@capitalbay.de>
2024-08-06 11:30:53 +0000

    Edited Map2_0.Mapbox_Functions

--- client_code/Map2_0/Mapbox_Functions.py
+++ client_code/Map2_0/Mapbox_Functions.py
@@ -12,7 +12,7 @@ def initialise_map(dom):
   return mapboxgl.Map(
     {
       'container': dom,
-      'style': "mapbox://styles/shinykampfkeule/clzia2dv200ii01qygs6v4rrc",
+      'style': "mapbox://styles/mapbox/light-v11",
       'center': [13.4092, 52.5167],
       'zoom': 8
     }
